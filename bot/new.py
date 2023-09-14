ca08066ff3affdf4be94c6b4862a9af0808a6a47
Stepan Denysenko <stepden04@gmail.com>
2023-09-14 14:07:29 +0200

    fixed sending after due time,fixed(?) deleting

--- bot/new.py
+++ bot/new.py
@@ -8,9 +8,9 @@ from dotenv import load_dotenv
 from course import Course, read_msg, load_cache, save_cache
 from threading import Thread, Timer
 from time import sleep
-from schedule import every, run_pending, clear, get_jobs, repeat, CancelJob
+from schedule import every, run_pending, clear, get_jobs, repeat
 from typing import List
-
+from pytz import timezone
 load_dotenv()
 
 ADMINS = os.getenv("ADMINS")
@@ -18,10 +18,11 @@ API_TOKEN = os.getenv("TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 POST_DELTA = int(os.getenv("POST_DELTA"))
 DELETE_DELTA = int(os.getenv("DELETE_DELTA"))
+SERVER_TZ = str(os.getenv("SERVER_TZ"))
 
 bot = TeleBot(API_TOKEN, parse_mode=None)
-test = 1
-
+kyiv = timezone("Europe/Kyiv")
+server = timezone(SERVER_TZ)
 class Single:
     courses = []
     leap_mod = True
@@ -35,27 +36,35 @@ class Single:
 try:
     main = Single()
     for course in load_cache()['courses']:
-        main.courses.append(Course(**course))    
+        main.courses.append(Course(**course))
 except Exception as e:
     print(e)
     print('cant read cache')
     main = Single()
 
+
+
 def notify(task: Course):
-    global test
-    try:
-        btn = InlineKeyboardMarkup()
-        btn.add(InlineKeyboardButton("Посилання на пару", task.get_link()))
-        main.msg = bot.send_message(chat_id=CHANNEL_ID,
-                         text=task,
-                         reply_markup=btn).message_id
-    except Exception as e:
-        print(e)
-        main.msg = bot.send_message(chat_id=CHANNEL_ID, text=f'{task}\n{task.get_link()}').message_id
-    try:
-        t = Timer(DELETE_DELTA*60, delete_last)  
-        t.start()
-    except: pass
+    print('sent')
+    now = server.localize(datetime.now())
+    job = datetime.strptime(task.time,"%H:%M")
+    job = kyiv.localize(job.replace(year=now.year,month=now.month,day=now.day,second=30))
+    print(now,job,job >= now)
+    if job >= now:
+        try:
+            btn = InlineKeyboardMarkup()
+            btn.add(InlineKeyboardButton("Посилання на пару", task.get_link()))
+            main.msg = bot.send_message(chat_id=CHANNEL_ID,
+                            text=task,
+                            reply_markup=btn).message_id
+        except Exception as e:
+            print(e)
+            main.msg = bot.send_message(chat_id=CHANNEL_ID, text=f'{task}\n{task.get_link()}').message_id
+
+        try:
+            t = Timer(DELETE_DELTA*60, delete_last)  
+            t.start()
+        except: pass
     
 
 def delete_last():
@@ -66,7 +75,8 @@ def delete_last():
         print(e)
 
 def init_schedule(course_list: List[Course]):
-    is_odd_week = int((int(datetime.today().strftime("%V")) + 0) % 2 and main.leap_mod)
+    is_odd_week = int((bool(datetime.today().strftime("%V")) % 2 ) and main.leap_mod)
+    print(is_odd_week)
     print('UPD')
     clear('daily')
     for course in course_list:
@@ -91,16 +101,15 @@ def get_table(message):
 
 @bot.message_handler(commands=['table'], func=user_filter)
 def get_table(message):
-    msg = ''
-    for course in main.courses:
-        if datetime.today().weekday() == course.day:
-            msg += (str(course) + '\n')
+    msg = '' 
+    for job in get_jobs('daily'):
+        msg += ((str(job.job_func.keywords['task'])) + '\n')
     bot.reply_to(message, msg)
 
 
 @bot.message_handler(commands=['flip'], func=user_filter)
 def flip_leap(message):
-    main.leap = not main.leap_mod
+    main.leap_mod = not main.leap_mod
     init_schedule(main.courses)
     bot.reply_to(message, 'Інвертовано верхній/нижній тижні')
 
@@ -131,6 +140,7 @@ def reset():
 if __name__ == "__main__":
     bot_polling = Thread(target=bot.infinity_polling)
     bot_polling.start()
+    init_schedule(main.courses)
     while True:
         run_pending()
         sleep(1)
